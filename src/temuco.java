1a9e0466b0be3621d50e027bbad8a3892e752a2b
manfunker <funkdust@gmail.com>
2014-09-15 20:58:27 -0300

    ufro

--- src/temuco.java
+++ src/temuco.java
@@ -8,12 +8,12 @@
  *
  * @author Manfunk
  */
-public class menu extends javax.swing.JFrame {
+public class temuco extends javax.swing.JFrame {
 
     /**
-     * Creates new form menu
+     * Creates new form temuco
      */
-    public menu() {
+    public temuco() {
         initComponents();
     }
 
@@ -26,77 +26,51 @@ public class menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
-        jButton9 = new javax.swing.JButton();
-        jButton10 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        jButton1.setText("Angol");
-        getContentPane().add(jButton1);
-        jButton1.setBounds(210, 190, 70, 55);
+        jButton2.setText("Menu");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(590, 470, 70, 23);
 
-        jButton2.setText("Cap. \nPastene");
-        jButton2.setToolTipText("");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jButton3.setText("Hospedaje");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(50, 310, 100, 50);
+
+        jButton4.setText("Museos");
+        getContentPane().add(jButton4);
+        jButton4.setBounds(210, 310, 100, 50);
+
+        jButton5.setText("Alrededores");
+        getContentPane().add(jButton5);
+        jButton5.setBounds(390, 310, 100, 50);
+
+        jButton6.setText("Entretención");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jButton6ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2);
-        jButton2.setBounds(310, 190, 100, 55);
-
-        jButton3.setText("Collipulli");
-        getContentPane().add(jButton3);
-        jButton3.setBounds(440, 190, 69, 55);
-
-        jButton4.setText("Lonquimay");
-        getContentPane().add(jButton4);
-        jButton4.setBounds(540, 190, 83, 55);
-
-        jButton5.setText("Pto. Saavedra");
-        getContentPane().add(jButton5);
-        jButton5.setBounds(270, 270, 130, 57);
-
-        jButton6.setText("Temuco ");
         getContentPane().add(jButton6);
-        jButton6.setBounds(440, 270, 73, 57);
+        jButton6.setBounds(540, 310, 110, 50);
 
-        jButton7.setText("villarrica");
-        getContentPane().add(jButton7);
-        jButton7.setBounds(550, 270, 71, 57);
-
-        jButton8.setText("Pucón");
-        getContentPane().add(jButton8);
-        jButton8.setBounds(170, 270, 70, 57);
-
-        jButton9.setText("About ");
-        getContentPane().add(jButton9);
-        jButton9.setBounds(10, 435, 65, 23);
-
-        jButton10.setText("Salir");
-        getContentPane().add(jButton10);
-        jButton10.setBounds(590, 430, 53, 23);
-
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Manfunk\\Desktop\\app turismo\\araucanía222222.jpg")); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 660, 470);
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Manfunk\\Desktop\\app turismo\\te.jpg")); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(0, 0, 680, 510);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_jButton6ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -115,35 +89,30 @@ public class menu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(temuco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(temuco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(temuco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(temuco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new menu().setVisible(true);
+                new temuco().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
-    private javax.swing.JButton jButton9;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
