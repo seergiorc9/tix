a69a304986d0db90d98ee70cfea65891afbd6855
Sergio Roa Campos <s.roa02@ufromail.cl>
2014-09-15 20:52:52 -0400

    Update Turismo.java

--- src/turismo/Turismo.java
+++ src/turismo/Turismo.java
@@ -5,7 +5,8 @@ public class Turismo {
     public static void main(String[] args) {
         //responder si vieron esta modificacion
         
-        
+        // 1234 copiado manu copiado,cambia y fuera
+    
     }
     
 }
